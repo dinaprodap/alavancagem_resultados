4ac0fe0a2f15fe58afc25c6e9d6386ab564bc4b9
Diná Beatriz Xavier <115733424+dinaprodap@users.noreply.github.com>
2025-02-21 18:35:44 -0300

    Update alavancagem.py
    ajuste custeio final

--- alavancagem.py
+++ alavancagem.py
@@ -279,10 +279,10 @@ with custeio_final_col1:
     custeio_final_mol1 = diferenciais["Molecula 1"] + custeio_mol1
     st.metric("Custeio Final (R$/Cab/dia) Mol 1", f"{custeio_final_mol1:.2f}")
 with custeio_final_col2:
-    custeio_final_mol2 = diferenciais["Molecula 1"] + custeio_mol2
+    custeio_final_mol2 = diferenciais["Molecula 2"] + custeio_mol2
     st.metric("Custeio Final (R$/Cab/dia) Mol 2", f"{custeio_final_mol2:.2f}")
 with custeio_final_col3:
-    custeio_final_mol3 = diferenciais["Molecula 1"] + custeio_mol3
+    custeio_final_mol3 = diferenciais["Molecula 3"] + custeio_mol3
     st.metric("Custeio Final (R$/Cab/dia) Mol 3", f"{custeio_final_mol3:.2f}")
     
 # Calcular valores para cada molécula
